b8378e96459a804a7e88cd5d4d6120f83fd53f1a
elyarenom <elya.renom@mail.mcgill.ca>
2023-07-12 05:57:57 -0400

    alignment v2
    edited alignment to use samtools and different reference genome

--- alignment.sh
+++ alignment.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
-bwa index GRCh38ref.gz
-bwa mem GRCh38ref.gz SRR12331326_GL000219.1.fastq > aligned.sam
+bwa mem \
+# invokes the BWA-MEM algorithm
+-t 8 \
+# Specifies the number of threads (parallel processes) to be used by BWA-MEM during the alignment
+-T 0 \
+# Sets the score threshold for a primary alignment
+-R <read_group> \
+/Users/adminelya/Documents/GitHub/NGS/data/reference\ genome/GRCh38ref.gz.sa \
+/Users/adminelya/Documents/GitHub/NGS/data/fastq/SRR12331326_GL000219.1.fastq |
+samtools view \
+-Shb
+-o /Users/adminelya/Documents/GitHub/NGS/data/aligned.sam
+# transforms to bam
+
