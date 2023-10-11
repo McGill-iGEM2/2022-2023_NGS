6a556e277abfc671204d75a64b556f619e7f9d03
jennifertramsu <jennifer.tramsu@gmail.com>
2023-10-11 10:06:42 -0700

    refactoring alignment

--- scripts/alignment.sh
+++ scripts/alignment.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Function for usage
+usage() {
+    echo -e "Usage: alignment.sh <-r ref> <-n left [right]> [-p] [-h]\n"
+            echo -e "Aligns sequencing data to the reference genome using BWA-MEM. The reference is indexed if not already.\n"
+            echo -e "Options:"    
+            echo -e "\t-r ref\t\tReference genome"
+            echo -e "\t-n left right\tSequencing data. If paired-end reads contained in two files, indicate the pair file with RIGHT"
+            echo -e "\t-p\t\tUse this flag to indicate that READS contains paired-end reads"
+            echo -e "\t-h\t\tHelp"
+}
+
+# Function for getting extra arguments
 function getopts-extra () {
     declare i=1
     # if the next argument is not an option, then append it to array OPTARG
@@ -9,59 +21,67 @@ function getopts-extra () {
     done
 }
 
+# Default parameter
 PAIRED=false
 
-while getopts "r:n:t:p" option; do
+while getopts "r:n:s:ph" option; do
     case $option in
-        r) ref=$OPTARG
-            if [ ! -f $ref.bwt ]; then
-                echo -e "Indexing reference genome...\n"
-                bwa index $ref
-                samtools faidx $ref
-            fi
-            ;;
-        p)  PAIRED=true
+        r)  ref=$OPTARG
             ;;
         n)  getopts-extra "$@"
             args=( "${OPTARG[@]}" )
-            n1=${args[0]}
-            n2=${args[1]}
-
-            n_file_path=$(dirname $n1)
-            echo -e "\nAligning normal sample to reference genome...\n"
-
-            if [ -z $n2 ] # only one file, paired ends concatenated
-            then
-                if $PAIRED
-                then
-                    bwa mem -p -M $ref $n1 > $n_file_path/n_aligned.sam
-                else
-                    bwa mem $ref $n1 > $n_file_path/n_aligned.sam
-                fi
-            else # two files
-                bwa mem $ref $n1 $n2 > $n_file_path/n_aligned.sam
-            fi
+            left=${args[0]}
+            right=${args[1]}
             ;;
-        t)  getopts-extra "$@"
-            args=( "${OPTARG[@]}" )
-            t1=${args[0]}
-            t2=${args[1]}
-
-            t_file_path=$(dirname $t1)
-            echo -e "\nAligning tumour sample to reference genome...\n"
-
-            if [ -z $t2 ]; then
-                if $PAIRED; 
-                then
-                    bwa mem -p -M $ref $t1 > $t_file_path/t_aligned.sam
-                else
-                    bwa mem $ref $t1 > $t_file_path/t_aligned.sam
-                fi
-            else
-                bwa mem $ref $t1 $t2 > $t_file_path/t_aligned.sam
-            fi
+        s)  sample=$OPTARG
+            ;;
+        p)  PAIRED=true
+            ;;
+        h) 
+            usage
+            exit 1
             ;;
     esac
 done
+
+# Error message if no arguments are given
+if [ $# -eq 0 ]; then
+    echo -e "Error: No arguments given\n"
+    usage
+    exit 1
+elif [ -z $ref ] || [ -z $left ]; then
+    echo -e "Error: Reference genome or sample not specified\n"
+    usage
+    exit 1
+elif [ -z $sample ]; then
+    echo -e "Error: Sample name not specified\n"
+    usage
+    exit 1
+fi
+
+# Extracting file path
+path=$(dirname $left)
+
+# Indexing reference genome if not already
+if [ ! -f $ref.bwt ]; then
+    echo -e "Indexing reference genome...\n"
+    bwa index $ref
+    samtools faidx $ref
+fi
+
+# Aligning sample to reference genome
+echo -e "\nAligning $sample to reference genome...\n"
+
+if [ -z $right ] # only one file, paired ends concatenated
+    then
+        if $PAIRED
+        then
+            bwa mem -p -M $ref $left > $path/${sample}_aligned.sam
+        else # single end reads
+            bwa mem $ref $left > $path/${sample}_aligned.sam
+        fi
+else # two files
+    bwa mem $ref $left $right > $path/${sample}_aligned.sam
+fi
 
 echo "Done!"
